8c27e15d374daf1148eab5e6d8868e31c6f255ca
Gabriel Freitas Souza <gabrielsouza45@live.com>
2024-08-29 00:09:26 -0300

    Adição de Enums no Angular

--- pharmasw-front/src/app/modelo/Usuario.ts
+++ pharmasw-front/src/app/modelo/Usuario.ts
@@ -1,7 +1,12 @@
+import { Grupo } from "./enums/Grupo";
+import { Status } from "./enums/Status";
+
 export class Usuario{
 
     nome: string;
     email: string;
     senha: string;
-
+    
+    grupo: Grupo;
+    status:Status;
 }
---
+++ pharmasw-front/src/app/modelo/enums/Grupo.ts
@@ -0,0 +1,4 @@
+export enum Grupo {
+    ADMINISTRADOR = 'Administrador',
+    ESTOQUISTA = 'Estoquista'
+  }
