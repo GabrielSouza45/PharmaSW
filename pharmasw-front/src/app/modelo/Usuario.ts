d10af07b5138cd769a42b7eae9991703fee6dd6b
AntonioJsNeto <ns4809623@gmail.com>
2024-08-29 22:37:52 -0300

    Criacao do cadastro no frontEnd

--- pharmasw-front/src/app/modelo/Usuario.ts
+++ pharmasw-front/src/app/modelo/Usuario.ts
@@ -6,7 +6,13 @@ export class Usuario{
     nome: string;
     email: string;
     senha: string;
-    
+    cpf: number;
+
     grupo: Grupo;
     status:Status;
+
+    dataIni: Date;
+    dataAlt: Date;
+    dataFim: Date;
+
 }
